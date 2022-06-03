5c3543c82c00c9fdf78ee48b2dc02ca007776c33
Dee <99658722+DeeJ4yNg@users.noreply.github.com>
2022-06-03 15:49:40 +0800

    Update and rename CheckComputerStatus3.py to CheckComputerStatus.py

--- CheckComputerStatus.py
+++ CheckComputerStatus.py
@@ -5,49 +5,47 @@ import tkinter.messagebox as messagebox
 
 def Read_CSV(Computer_Obj):
     #read computer info csv
-    csv = pd.read_csv(r'\\cnfuosms02\SecurityUpdateSelfCheck\AD_CMDB_OPP_Acrobat.csv')
+    csv = pd.read_csv('./Computer_Status.csv')
     Computer_Obj = Computer_Obj.upper()
-    Computer = csv[csv['AD_Machine'] == Computer_Obj]
+    Computer = csv[csv['Machine'] == Computer_Obj]
     Computer = Computer.fillna('-')
     #read patch status csv
-    csv_patch_info = pd.read_csv(r'\\cnfuosms02\SecurityUpdateSelfCheck\Patch_Deployment_Status.csv')
+    csv_patch_info = pd.read_csv('./Compliance_Status.csv')
     Computer_patch = csv_patch_info[csv_patch_info['ComputerName'] == Computer_Obj]
     failed_patches = []
     for i in Computer_patch.values:
         if i[1] == '2' or i[1] == '0':
             failed_patches.append(i[3])
 
-    if Computer['AD_Machine'].empty:
-        messagebox.showerror('Computer Info Checking Tool – KPMG SCCM',
+    if Computer['Machine'].empty:
+        messagebox.showerror('Computer Info Checking Tool',
                 			'Error, Computer was not found.')
     else:
-        Computer_Name = Computer['AD_Machine'].values[0]
+        Computer_Name = Computer['Machine'].values[0]
         Computer_Account_Status = 'True' if Computer['Enabled'].values[0] == 1 else 'False'
-        Model = Computer['CMDB_Model_Desc'].values[0]
-        User = Computer['CMDB_display_name'].values[0]
+        Model = Computer['Model'].values[0]
+        User = Computer['User_name'].values[0]
         User_IID = Computer['User_IID'].values[0]
         Main_OU = Computer['OU'].values[0]
-        Office = Computer['SubOU'].values[0]
+        Office = Computer['ADOU'].values[0]
         Grade = Computer['Grade'].values[0]
-        CMDB_Status = Computer['CMDB_Status'].values[0]
-        GD = Computer['GD'].values[0]
-        Office_365_Version = Computer['OPPVersion'].values[0]
-        Acrobat_Version = Computer['AcrobatVersion'].values[0]
+        CMDB_Status = Computer['Status'].values[0]
+        GD = Computer['System'].values[0]
+        Office_365_Version = Computer['Version0'].values[0]
+        Acrobat_Version = Computer['Version1'].values[0]
 
-        if Office != 'Servicing01' and Office != 'Servicing02' and Office != 'Servicing03':
+        if Office != 'S01' and Office != 'S02':
             Restriction = '-'
-        elif Office == 'Servicing01':
-            Restriction = 'Restriction level 1, enabled watermark and reminder'
-        elif Office == 'Servicing02':
-            Restriction = 'Restriction level 2, enabled watermark and reminder, restricted browsers'
-        else:
-            Restriction = 'Restriction level 3, enabled watermark and reminder, restricted browsers, Office and Acrobat applications'
-
-        AD_Description = Computer['AD_Description'].values[0]
-        CMDB_Remark = Computer['ci_remarks'].values[0]
+        elif Office == 'S01':
+            Restriction = 'Restriction level 1'
+        elif Office == 'S02':
+            Restriction = 'Restriction level 2'
+       
+        AD_Description = Computer['Description'].values[0]
+        CMDB_Remark = Computer['remarks'].values[0]
         Computer_Patch_Status = failed_patches
 
-    return Computer_Name,Computer_Account_Status,str(Model).replace(" ", "\ "),str(User).replace(" ", "\ "),User_IID,Main_OU,Office,Grade,CMDB_Status,GD,Office_365_Version,Acrobat_Version,str(Restriction).replace(" ","\ "),str(AD_Description).replace(" ","\ "), str(CMDB_Remark).replace(" ","\ "), Computer_Patch_Status
+    return Computer_Name,Computer_Account_Status,str(Model).replace(" ", "\ "),str(User).replace(" ", "\ "),ID,OU,Office,Grade,Status,System,Version0,Version1,str(Restriction).replace(" ","\ "),str(Description).replace(" ","\ "), str(Remark).replace(" ","\ "), Compliance_Status
 
 class POPUP():
     def __init__(self):
@@ -96,24 +94,23 @@ class POPUP():
         tree.insert("", 0, text="Computer_Name", values=info[0])
         tree.insert("", 1, text="Computer_Account_Status", values=info[1])
         tree.insert("", 2, text="Model", values=info[2])
-        tree.insert("", 3, text="Main_OU", values=info[5])
-        tree.insert("", 4, text="Sub_OU", values=info[6])
-        tree.insert("", 5, text="CMDB_Status", values=info[8])
+        tree.insert("", 3, text="OU", values=info[5])
+        tree.insert("", 4, text="Office", values=info[6])
+        tree.insert("", 5, text="Status", values=info[8])
         tree.insert("", 6, text="User", values=info[3])
-        tree.insert("", 7, text="User_IID", values=info[4])
+        tree.insert("", 7, text="ID", values=info[4])
         tree.insert("", 8, text="Grade", values=info[7])
-        tree.insert("", 9, text="GD", values=info[9], tags='tag_blue')
-        tree.insert("", 10, text="Office_365_Version", values=info[10], tags='tag_blue')
-        tree.insert("", 11, text="Acrobat_Version", values=info[11], tags='tag_blue')
+        tree.insert("", 9, text="System", values=info[9], tags='tag_blue')
+        tree.insert("", 10, text="Version0", values=info[10], tags='tag_blue')
+        tree.insert("", 11, text="Version1", values=info[11], tags='tag_blue')
         tree.insert("", 12, text="Restriction", values=info[12])
-        tree.insert("", 13, text="AD_Description", values=info[13])
-        tree.insert("", 14, text="CMDB_Remark", values=info[14])
-        #tree.insert("", 13, text="Computer_Missing_Patches", values=info[13])
+        tree.insert("", 13, text="Description", values=info[13])
+        tree.insert("", 14, text="Remark", values=info[14])
         Missing_Patches_Count = 14
         for j in info[15]:
             Missing_Patches_Count += 1
             tree.insert("", Missing_Patches_Count, text="Missing_Security_Patch", values=str(j).replace(" ", "\ "))
-        if info[6] == 'Servicing01' or info[6] == 'Servicing02' or info[6] == 'Servicing03':
+        if info[6] == 'S01' or info[6] == 'S02':
             items = list(tree.get_children())
             tree.item(items[12], tag='tag')
         tree.pack()
@@ -122,5 +119,3 @@ class POPUP():
 
 a = POPUP()
 a.TKWindow()
-#print(Read_CSV('cnpc0gh2ve'))
-#CNPC1XF068
